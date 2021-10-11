3d0d2632fb6732c3394dcf44a1041873a5900476
min9288 <76557412+min9288@users.noreply.github.com>
2021-10-11 18:59:10 +0900

    ..

---
+++ DB/2021-10-11/webserver.sql
@@ -0,0 +1,10 @@
+CREATE TABLE PHOTO(
+    PHOTO_NO        NUMBER          PRIMARY KEY,            --  사진게시판 관리번호
+    PHOTO_WRITER    VARCHAR2(20)    REFERENCES MEMBER(MEMBER_ID) ON DELETE CASCADE, -- 작성자
+    PHOTO_COMMENT   VARCHAR2(100)   NOT NULL,       -- 사진에 대한 설명
+    PHOTO_DATE      CHAR(10),                       -- 작성일
+    FILEPATH        VARCHAR2(100)                   -- 이미지 파일 업로드 경로
+);
+CREATE SEQUENCE PHOTO_SEQ;
+
+select * from photo;
